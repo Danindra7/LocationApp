a7108708619216a3c7c1840ff0fe555de8c1112e
Danindra7 <56295369+Danindra7@users.noreply.github.com>
2021-07-03 21:03:13 +0700

    update
    -

--- src/RootScreen.js
+++ src/RootScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { PermissionsAndroid } from 'react-native';
 
@@ -8,27 +8,27 @@ import ReactNativeForegroundService from '@supersami/rn-foreground-service';
 import Geolocation from '@react-native-community/geolocation';
 import GetLocation from 'react-native-get-location'
 
-RNLocation.configure({
-    distanceFilter: 0, // Meters
-    desiredAccuracy: {
-        ios: 'best',
-        android: 'balancedPowerAccuracy',
-    },
-    // Android only
-    androidProvider: 'auto',
-    interval: 5000, // Milliseconds
-    fastestInterval: 1000, // Milliseconds
-    maxWaitTime: 5000, // Milliseconds
-    // iOS Only
-    // activityType: 'other',
-    // allowsBackgroundLocationUpdates: false,
-    // headingFilter: 1, // Degrees
-    // headingOrientation: 'portrait',
-    // pausesLocationUpdatesAutomatically: false,
-    // showsBackgroundLocationIndicator: false,
-});
-let locationSubscription = null;
-let locationTimeout = null;
+// RNLocation.configure({
+//     distanceFilter: 0, // Meters
+//     desiredAccuracy: {
+//         ios: 'best',
+//         android: 'balancedPowerAccuracy',
+//     },
+//     // Android only
+//     androidProvider: 'auto',
+//     interval: 5000, // Milliseconds
+//     fastestInterval: 1000, // Milliseconds
+//     maxWaitTime: 5000, // Milliseconds
+//     // iOS Only
+//     // activityType: 'other',
+//     // allowsBackgroundLocationUpdates: false,
+//     // headingFilter: 1, // Degrees
+//     // headingOrientation: 'portrait',
+//     // pausesLocationUpdatesAutomatically: false,
+//     // showsBackgroundLocationIndicator: false,
+// });
+// let locationSubscription = null;
+// let locationTimeout = null;
 
 
 
@@ -40,103 +40,117 @@ const RootScreen = () => {
     const [info, setInfo] = useState([]);
     const [lat, setLat] = useState('');
 
+    const requestPermission = async () => {
+
+        //request the permission before starting the service.
+        const backgroundgranted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+            {
+                title: 'Background Location Permission',
+                message:
+                    'We need access to your location ' +
+                    'so you can get live quality updates.',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+        if (backgroundgranted === PermissionsAndroid.RESULTS.GRANTED) {
+            //do your thing!
+            console.log("permission granted sir")
+
+            const interval = setInterval(async () => {
+                console.log('Logs every minute');
+                await GetLocation.getCurrentPosition({
+                    enableHighAccuracy: true,
+                    timeout: 15000,
+                })
+                    .then(location => {
+                        console.log(location);
+                        const longitude = location.longitude;
+                        const latitude = location.latitude;
+                        const data = {
+                            longitude: longitude,
+                            latitude: latitude,
+                        }
+                        const newInfo = info.concat(data)
+                        setInfo(newInfo)
+                        setLat(location.latitude)
+                    })
+                    .catch(error => {
+                        const { code, message } = error;
+                        console.warn(code, message);
+                    })
+            }, 5000);
+
+            // Geolocation.getCurrentPosition(data => {
+            //     setLong(data.coords.latitude);
+            // })
+            // ReactNativeForegroundService.add_task(
+            //     () => {
+            //         RNLocation.requestPermission({
+            //             ios: 'whenInUse',
+            //             android: {
+            //                 detail: 'fine',
+            //             },
+            //         }).then((granted) => {
+            //             console.log('Location Permissions: ', granted);
+            //             // if has permissions try to obtain location with RN location
+            //             if (granted === true) {
+            //                 locationSubscription && locationSubscription();
+            //                 locationSubscription = RNLocation.subscribeToLocationUpdates(
+            //                     ([locations]) => {
+            //                         locationSubscription();
+            //                         locationTimeout && clearTimeout(locationTimeout);
+            //                         console.log(JSON.stringify(locations));
+            //                         console.log(JSON.parse(locations));
+            //                         setLong(locations);
+            //                     },
+            //                 );
+            //             } else {
+            //                 locationSubscription && locationSubscription();
+            //                 locationTimeout && clearTimeout(locationTimeout);
+            //                 console.log('no permissions to obtain location');
+            //             }
+            //         });
+            //     },
+            //     {
+            //         delay: 1000,
+            //         onLoop: true,
+            //         taskId: 'taskid',
+            //         onError: (e) => console.log('Error logging:', e),
+            //     },
+            // );
+
+
+        }
+    }
+
     useEffect(() => {
 
-        const requestPermission = async () => {
 
-            //request the permission before starting the service.
-            const backgroundgranted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
-                {
-                    title: 'Background Location Permission',
-                    message:
-                        'We need access to your location ' +
-                        'so you can get live quality updates.',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                },
-            );
-            if (backgroundgranted === PermissionsAndroid.RESULTS.GRANTED) {
-                //do your thing!
-                console.log("permission granted sir")
-
-                const interval = setInterval(async () => {
-                    console.log('Logs every minute');
-                    await GetLocation.getCurrentPosition({
-                        enableHighAccuracy: true,
-                        timeout: 15000,
-                    })
-                        .then(location => {
-                            console.log(location);
-                            const longitude = location.longitude;
-                            const latitude = location.latitude;
-                            const data = {
-                                longitude: longitude,
-                                latitude: latitude,
-                            }
-                            const newInfo = info.concat(data)
-                            setInfo(newInfo)
-                            setLat(location.latitude)
-                        })
-                        .catch(error => {
-                            const { code, message } = error;
-                            console.warn(code, message);
-                        })
-                }, 5000);
-
-                // Geolocation.getCurrentPosition(data => {
-                //     setLong(data.coords.latitude);
-                // })
-                // ReactNativeForegroundService.add_task(
-                //     () => {
-                //         RNLocation.requestPermission({
-                //             ios: 'whenInUse',
-                //             android: {
-                //                 detail: 'fine',
-                //             },
-                //         }).then((granted) => {
-                //             console.log('Location Permissions: ', granted);
-                //             // if has permissions try to obtain location with RN location
-                //             if (granted === true) {
-                //                 locationSubscription && locationSubscription();
-                //                 locationSubscription = RNLocation.subscribeToLocationUpdates(
-                //                     ([locations]) => {
-                //                         locationSubscription();
-                //                         locationTimeout && clearTimeout(locationTimeout);
-                //                         console.log(JSON.stringify(locations));
-                //                         console.log(JSON.parse(locations));
-                //                         setLong(locations);
-                //                     },
-                //                 );
-                //             } else {
-                //                 locationSubscription && locationSubscription();
-                //                 locationTimeout && clearTimeout(locationTimeout);
-                //                 console.log('no permissions to obtain location');
-                //             }
-                //         });
-                //     },
-                //     {
-                //         delay: 1000,
-                //         onLoop: true,
-                //         taskId: 'taskid',
-                //         onError: (e) => console.log('Error logging:', e),
-                //     },
-                // );
-
-
-            }
-        }
-        requestPermission();
+        // requestPermission();
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <View>
-            <Text>Halo CUAKS</Text>
+            {/* <Text>Halo </Text> */}
             {console.log(info)}
-            <Text> {lat} </Text>
+            {info.length > 0 ?
+                info.map((data, idx) => {
+                    return (
+                        <View key={idx} style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <Text>{idx + 1}. </Text>
+                            <Text>longitude : {data.longitude}</Text>
+                            <Text>latitude :{data.latitude}</Text>
+                        </View>
+                    )
+                }) :
+                <Text>No Data</Text>
+            }
+            <Button title="get location" onPress={requestPermission}></Button>
         </View>
     );
 };
